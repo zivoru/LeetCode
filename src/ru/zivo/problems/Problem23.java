843128141ce3100607b7cd0ce0a3babc203506d3
zivo <zivoru@gmail.com>
2022-08-16 20:15:43 +0500

    Java Solution for 23

---
+++ src/ru/zivo/problems/Problem23.java
@@ -0,0 +1,48 @@
+package ru.zivo.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Source : https://leetcode.com/problems/merge-k-sorted-lists
+ * Author : Zimin Vladimir
+ * Date   : 2022-08-16
+ */
+public class Problem23 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (ListNode listNode : lists) {
+            while (listNode != null) {
+                list.add(listNode.val);
+                listNode = listNode.next;
+            }
+        }
+
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        List<Integer> integers = list.stream().sorted().toList();
+        for (Integer integer : integers) {
+            curr.next = new ListNode(integer);
+            curr = curr.next;
+        }
+
+        return dummyHead.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
