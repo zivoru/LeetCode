9e912aa57452c63d477af6ac0334b1b50623cee9
zivo <zivoru@gmail.com>
2022-08-17 16:57:33 +0500

    Optimization for 804

--- src/ru/zivo/problems/Problem804.java
+++ src/ru/zivo/problems/Problem804.java
@@ -1,8 +1,6 @@
 package ru.zivo.problems;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -14,40 +12,40 @@ public class Problem804 {
     public int uniqueMorseRepresentations(String[] words) {
         if (words.length == 1) return 1;
 
-        Map<Character, String> map = new HashMap<>();
-        map.put('a', ".-");
-        map.put('b', "-...");
-        map.put('c', "-.-.");
-        map.put('d', "-..");
-        map.put('e', ".");
-        map.put('f', "..-.");
-        map.put('g', "--.");
-        map.put('h', "....");
-        map.put('i', "..");
-        map.put('j', ".---");
-        map.put('k', "-.-");
-        map.put('l', ".-..");
-        map.put('m', "--");
-        map.put('n', "-.");
-        map.put('o', "---");
-        map.put('p', ".--.");
-        map.put('q', "--.-");
-        map.put('r', ".-.");
-        map.put('s', "...");
-        map.put('t', "-");
-        map.put('u', "..-");
-        map.put('v', "...-");
-        map.put('w', ".--");
-        map.put('x', "-..-");
-        map.put('y', "-.--");
-        map.put('z', "--..");
+        String[] array = new String[123];
+        array[97] = ".-";
+        array[98] = "-...";
+        array[99] = "-.-.";
+        array[100] = "-..";
+        array[101] = ".";
+        array[102] = "..-.";
+        array[103] = "--.";
+        array[104] = "....";
+        array[105] = "..";
+        array[106] = ".---";
+        array[107] = "-.-";
+        array[108] = ".-..";
+        array[109] = "--";
+        array[110] = "-.";
+        array[111] = "---";
+        array[112] = ".--.";
+        array[113] = "--.-";
+        array[114] = ".-.";
+        array[115] = "...";
+        array[116] = "-";
+        array[117] = "..-";
+        array[118] = "...-";
+        array[119] = ".--";
+        array[120] = "-..-";
+        array[121] = "-.--";
+        array[122] = "--..";
 
         Set<String> strings = new HashSet<>();
 
         for (String word : words) {
             StringBuilder stringBuilder = new StringBuilder();
             for (int i = 0; i < word.length(); i++) {
-                stringBuilder.append(map.get(word.charAt(i)));
+                stringBuilder.append(array[word.charAt(i)]);
             }
             strings.add(stringBuilder.toString());
         }
