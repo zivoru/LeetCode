51e53636b57b0c389bd71f4bbb546824c0921347
zivo <zivoru@gmail.com>
2022-08-18 01:07:27 +0500

    Optimization for 4

--- src/ru/zivo/problems/Problem4.java
+++ src/ru/zivo/problems/Problem4.java
@@ -1,8 +1,5 @@
 package ru.zivo.problems;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Source : https://leetcode.com/problems/median-of-two-sorted-arrays
  * Author : Zimin Vladimir
@@ -10,20 +7,28 @@ import java.util.List;
  */
 public class Problem4 {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        List<Integer> nums = new ArrayList<>();
+        int[] ints = new int[nums1.length + nums2.length];
 
-        for (int i : nums1) {
-            nums.add(i);
-        }
-        for (int i : nums2) {
-            nums.add(i);
+        for (int i = 0, j = 0, k = 0; i < nums1.length || j < nums2.length; k++) {
+            if (i < nums1.length && j < nums2.length) {
+                if (nums1[i] < nums2[j]) {
+                    ints[k] = nums1[i];
+                    i++;
+                } else {
+                    ints[k] = nums2[j];
+                    j++;
+                }
+            } else if (i < nums1.length) {
+                ints[k] = nums1[i];
+                i++;
+            } else {
+                ints[k] = nums2[j];
+                j++;
+            }
         }
 
-        List<Integer> sortedNums = nums.stream().sorted().toList();
-        if (sortedNums.size() % 2 != 0) {
-            return sortedNums.get((int) (sortedNums.size() * 0.5));
-        }
-        return ((double) sortedNums.get((int) (sortedNums.size() * 0.5))
-                + sortedNums.get((int) (sortedNums.size() * 0.5 - 1))) / 2;
+        return ints.length % 2 != 0
+                ? ints[(int) (ints.length * 0.5)]
+                : ((double) ints[(int) (ints.length * 0.5)] + ints[(int) (ints.length * 0.5 - 1)]) / 2;
     }
 }
