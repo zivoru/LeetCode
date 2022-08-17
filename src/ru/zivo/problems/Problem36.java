e6d000b4a4bb13c5947d3584a53148ae3f25be19
zivo <zivoru@gmail.com>
2022-08-17 19:34:25 +0500

    Optimization for 36

--- src/ru/zivo/problems/Problem36.java
+++ src/ru/zivo/problems/Problem36.java
@@ -10,8 +10,19 @@ import java.util.Map;
  */
 public class Problem36 {
     public boolean isValidSudoku(char[][] board) {
+        Map<Character, Byte> square1 = new HashMap<>();
+        Map<Character, Byte> square2 = new HashMap<>();
+        Map<Character, Byte> square3 = new HashMap<>();
+
         for (int i = 0; i < board.length; i++) {
+            if (i == 3 || i == 6) {
+                square1.clear();
+                square2.clear();
+                square3.clear();
+            }
+
             Map<Character, Byte> map = new HashMap<>();
+
             for (int j = 0; j < board[i].length; j++) {
                 if (board[i][j] != '.') {
                     if (map.containsKey(board[i][j])) {
@@ -19,47 +30,34 @@ public class Problem36 {
                     } else {
                         map.put(board[i][j], (byte) 1);
                     }
-                    for (int k = i + 1; k < board.length; k++) {
-                        if (board[k][j] == board[i][j]) {
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
 
-        for (int k = 0; k < 7; k+=3) {
-            Map<Character, Byte> square1 = new HashMap<>();
-            Map<Character, Byte> square2 = new HashMap<>();
-            Map<Character, Byte> square3 = new HashMap<>();
-
-            for (int i = k; i < k + 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    if (board[i][j] != '.') {
+                    if (j == 0 || j == 1 || j == 2) {
                         if (square1.containsKey(board[i][j])) {
                             return false;
                         } else {
                             square1.put(board[i][j], (byte) 1);
                         }
                     }
-                }
 
-                for (int j = 3; j < 6; j++) {
-                    if (board[i][j] != '.') {
+                    if (j == 3 || j == 4 || j == 5) {
                         if (square2.containsKey(board[i][j])) {
                             return false;
                         } else {
                             square2.put(board[i][j], (byte) 1);
                         }
                     }
-                }
 
-                for (int j = 6; j < 9; j++) {
-                    if (board[i][j] != '.') {
+                    if (j == 6 || j == 7 || j == 8) {
                         if (square3.containsKey(board[i][j])) {
                             return false;
                         } else {
                             square3.put(board[i][j], (byte) 1);
+                        }
+                    }
+
+                    for (int k = i + 1; k < board.length; k++) {
+                        if (board[k][j] == board[i][j]) {
+                            return false;
                         }
                     }
                 }
