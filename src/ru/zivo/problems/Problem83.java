7a4b1e2a56eee49c858cbc9be5b45ccd815d0a4d
zivo <zivoru@gmail.com>
2022-08-20 02:15:09 +0500

    Java Solution for 83

---
+++ src/ru/zivo/problems/Problem83.java
@@ -0,0 +1,26 @@
+package ru.zivo.problems;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Source : https://leetcode.com/problems/remove-duplicates-from-sorted-list
+ * Author : Zimin Vladimir
+ * Date   : 2022-08-20
+ */
+public class Problem83 {
+    public ListNode deleteDuplicates(ListNode head) {
+        Set<Integer> values = new HashSet<>();
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        while (head != null) {
+            if (!values.contains(head.val)) {
+                curr.next = new ListNode(head.val);
+                curr = curr.next;
+                values.add(head.val);
+            }
+            head = head.next;
+        }
+        return dummyHead.next;
+    }
+}
