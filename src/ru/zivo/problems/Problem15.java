5a2c0efd632e945ed8fa0cb5d83c36e99762a2d1
zivo <zivoru@gmail.com>
2022-08-21 14:42:42 +0500

    Java Solution for 15

---
+++ src/ru/zivo/problems/Problem15.java
@@ -0,0 +1,31 @@
+package ru.zivo.problems;
+
+import java.util.*;
+
+/**
+ * Source : https://leetcode.com/problems/3sum
+ * Author : Zimin Vladimir
+ * Date   : 2022-08-21
+ */
+public class Problem15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> sums = new HashSet<>();
+        Map<Integer, Integer> dummyNums = new HashMap<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = nums.length - 1; j > i; j--) {
+                Integer integer = dummyNums.get(-(nums[i] + nums[j]));
+                if (integer != null) {
+                    if (integer != i && integer != j) {
+                        List<Integer> list = new ArrayList<>(List.of(nums[i],nums[j],-(nums[i] + nums[j])));
+                        Collections.sort(list);
+                        sums.add(list);
+                    }
+                }
+                dummyNums.put(nums[j], j);
+            }
+        }
+
+        return new ArrayList<>(sums);
+    }
+}
